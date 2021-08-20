4ccefe3588075644daaf345bd7a49a7d23c6f958
StoneHanaMori <18376333@buaa.edu.cn>
2021-08-20 17:29:13 +0800

    部署时的settings修改

--- MoyuBackend/settings.py
+++ MoyuBackend/settings.py
@@ -143,6 +143,12 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+# 前端上线后的配置
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "moyu-frontend/dist/")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
