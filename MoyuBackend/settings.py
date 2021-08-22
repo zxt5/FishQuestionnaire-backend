29bc5e2f8a3332594c36009568c6b6f894de22d6
StoneHanaMori <18376333@buaa.edu.cn>
2021-08-22 20:43:50 +0800

    增加搜索功能

--- MoyuBackend/settings.py
+++ MoyuBackend/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 3,
-    # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
     # 'DEFAULT_FILTER_BACKENDS':
     #     ['django_filters.rest_framework.DjangoFilterBackend'],
